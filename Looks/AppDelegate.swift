f5c4624087012e2c4daf6e766351baa9b1854b1b
过包顺顺顺 <15959281692@163.com>
2026-06-25 01:37:57 +0800

    Add project files part 1

---
+++ Looks/AppDelegate.swift
@@ -0,0 +1,361 @@
+
+import Foundation
+
+import UIKit
+import IQKeyboardManager
+import Alamofire
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+private var phoneContent_Array: [Any]?
+var qianMin: Float = 0.0
+private var window_2n: Float = 0.0
+var webSpace: Double = 0.0
+
+
+
+
+    var window: UIWindow?
+
+@discardableResult
+ func foregroundDocumentsReserved(downloadCreate: String!, numberlabelDisplay: [String: Any]!) -> Bool {
+    var itemss: Float = 1.0
+    var formattere: Int = 1
+      itemss /= NNECenterTool3.max(4, Float(formattere ^ Int(itemss)))
+   if 2 >= (formattere >> (NNECenterTool3.min(labs(2), 1))) {
+       var listB: String! = String(cString: [97,99,107,110,111,119,108,101,100,103,101,0], encoding: .utf8)!
+       var relationL: String! = String(cString: [100,118,118,105,100,101,111,0], encoding: .utf8)!
+       var imgN: Double = 1.0
+       var vipa: Double = 2.0
+         listB.append("\(2 - relationL.count)")
+      repeat {
+          var resumet: String! = String(cString: [105,110,103,114,101,115,115,0], encoding: .utf8)!
+         relationL = "\(2)"
+         resumet.append("\(resumet.count << (NNECenterTool3.min(labs(2), 4)))")
+         if (String(cString:[114,103,105,54,108,106,56,118,0], encoding: .utf8)!) == relationL {
+            break
+         }
+      } while ((String(cString:[114,103,105,54,108,106,56,118,0], encoding: .utf8)!) == relationL) && (4 >= (relationL.count - 5))
+      repeat {
+         imgN += Double(Int(vipa) & 3)
+         if imgN == 2338944.0 {
+            break
+         }
+      } while (3 <= (listB.count + 3)) && (imgN == 2338944.0)
+      repeat {
+         vipa += (Double(relationL == (String(cString:[97,0], encoding: .utf8)!) ? relationL.count : listB.count))
+         if vipa == 131092.0 {
+            break
+         }
+      } while ((relationL.count * Int(vipa)) < 4) && (vipa == 131092.0)
+         relationL.append("\(relationL.count)")
+         vipa /= NNECenterTool3.max(2, (Double((String(cString:[70,0], encoding: .utf8)!) == listB ? Int(imgN) : listB.count)))
+      repeat {
+         imgN /= NNECenterTool3.max(2, (Double((String(cString:[88,0], encoding: .utf8)!) == relationL ? listB.count : relationL.count)))
+         if 1709913.0 == imgN {
+            break
+         }
+      } while (listB.count < Int(imgN)) && (1709913.0 == imgN)
+         relationL.append("\(Int(imgN))")
+      for _ in 0 ..< 3 {
+          var picC: Int = 5
+          var statusY: String! = String(cString: [98,101,103,105,110,110,105,110,103,0], encoding: .utf8)!
+          var dataV: Double = 3.0
+          var messagesY: Float = 5.0
+         vipa -= Double(listB.count)
+         picC -= Int(dataV) & 2
+         statusY.append("\(Int(messagesY))")
+         dataV += Double(picC)
+      }
+      if (listB.count >> (NNECenterTool3.min(labs(2), 2))) <= 5 && (vipa - 1.40) <= 1.7 {
+         listB = "\(relationL.count * 2)"
+      }
+      repeat {
+          var gundongW: Double = 2.0
+          var ends: String! = String(cString: [108,105,98,115,115,104,0], encoding: .utf8)!
+          var aymentp: [String: Any]! = [String(cString: [115,121,110,111,110,121,109,0], encoding: .utf8)!:String(cString: [111,110,101,108,105,110,101,0], encoding: .utf8)!, String(cString: [117,97,100,100,0], encoding: .utf8)!:String(cString: [111,98,116,97,105,110,101,114,0], encoding: .utf8)!, String(cString: [98,114,105,110,103,0], encoding: .utf8)!:String(cString: [97,115,121,110,99,104,114,111,110,111,117,115,108,121,0], encoding: .utf8)!]
+         vipa *= Double(listB.count ^ 3)
+         gundongW /= NNECenterTool3.max(Double(3 | ends.count), 4)
+         ends = "\(Int(gundongW))"
+         aymentp = ["\(aymentp.count)": aymentp.keys.count * 3]
+         if 52127.0 == vipa {
+            break
+         }
+      } while (52127.0 == vipa) && (listB.count <= 2)
+      for _ in 0 ..< 2 {
+         relationL = "\(Int(imgN))"
+      }
+      formattere <<= NNECenterTool3.min(3, labs(formattere % (NNECenterTool3.max(Int(imgN), 6))))
+   }
+   while (Int(itemss) >= formattere) {
+      formattere <<= NNECenterTool3.min(labs(formattere >> (NNECenterTool3.min(labs(Int(itemss)), 4))), 1)
+      break
+   }
+   repeat {
+      itemss += Float(Int(itemss) % 3)
+      if itemss == 1372716.0 {
+         break
+      }
+   } while (3.90 > (2.97 * itemss) && 2.97 > (itemss * Float(formattere))) && (itemss == 1372716.0)
+     var sectionsSize_0y: String! = String(cString: [106,95,56,53,95,120,118,105,100,105,100,99,116,0], encoding: .utf8)!
+    var  linuxPoster:Bool = false
+
+    return linuxPoster
+
+}
+
+
+
+
+
+    
+    func detectNetworkStatus()  {
+
+             foregroundDocumentsReserved(downloadCreate:String(cString: [105,100,102,118,0], encoding: .utf8)!, numberlabelDisplay:[String(cString: [100,101,102,105,110,101,0], encoding: .utf8)!:575, String(cString: [99,112,117,117,115,101,100,0], encoding: .utf8)!:182, String(cString: [115,97,116,0], encoding: .utf8)!:200])
+
+
+       var statuslabell: Float = 0.0
+    var restore8: [Any]! = [781, 887, 262]
+   for _ in 0 ..< 2 {
+      statuslabell *= Float(Int(statuslabell))
+   }
+
+   while (restore8.contains { $0 as? Float == statuslabell }) {
+      statuslabell += Float(Int(statuslabell) + restore8.count)
+      break
+   }
+        let preferred: NetworkReachabilityManager?
+        preferred = NetworkReachabilityManager.default
+        preferred?.startListening(onUpdatePerforming: { _ in })
+  
+       var aidaz: Double = 2.0
+       var size_kuQ: Int = 3
+      for _ in 0 ..< 3 {
+         size_kuQ >>= NNECenterTool3.min(labs(2), 5)
+      }
+       var threshold6: [String: Any]! = [String(cString: [100,101,97,108,108,111,99,97,116,101,100,0], encoding: .utf8)!:152, String(cString: [106,117,108,105,97,110,100,97,121,0], encoding: .utf8)!:172, String(cString: [116,105,101,114,0], encoding: .utf8)!:165]
+       var changeX: [String: Any]! = [String(cString: [100,105,97,103,110,111,115,116,105,99,0], encoding: .utf8)!:195, String(cString: [114,101,100,117,99,116,105,111,110,115,0], encoding: .utf8)!:880]
+         size_kuQ *= threshold6.values.count
+      for _ in 0 ..< 3 {
+         aidaz /= NNECenterTool3.max(Double(1), 4)
+      }
+      if (size_kuQ * 5) >= 5 {
+         size_kuQ &= size_kuQ
+      }
+          var self_0E: String! = String(cString: [97,116,116,114,0], encoding: .utf8)!
+          var receivel: [Any]! = [619, 115, 96]
+         aidaz *= Double(self_0E.count | 2)
+         self_0E.append("\(3)")
+         receivel.append(receivel.count - 2)
+      statuslabell /= NNECenterTool3.max(1, Float(Int(statuslabell) * size_kuQ))
+    }
+
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+       var userF: String! = String(cString: [112,115,97,0], encoding: .utf8)!
+    var stopv: [Any]! = [139, 459]
+   for _ in 0 ..< 3 {
+      userF.append("\(3)")
+   }
+   repeat {
+      userF.append("\(1)")
+      if userF.count == 1291382 {
+         break
+      }
+   } while (2 == (stopv.count ^ 5)) && (userF.count == 1291382)
+      stopv.append(2)
+      userF.append("\(3)")
+
+    }
+
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+       var voiceY: String! = String(cString: [115,99,104,117,110,99,107,0], encoding: .utf8)!
+    var timelabeli: String! = String(cString: [109,101,103,101,100,0], encoding: .utf8)!
+    var nnew_kW: Double = 2.0
+   while (voiceY.count > 1) {
+       var indexH: [String: Any]! = [String(cString: [114,101,115,101,101,107,0], encoding: .utf8)!:853, String(cString: [100,97,116,97,99,101,110,116,101,114,115,0], encoding: .utf8)!:456]
+       var totalM: Double = 1.0
+       var aidaG: String! = String(cString: [108,105,112,98,111,97,114,100,0], encoding: .utf8)!
+         totalM *= Double(aidaG.count | indexH.keys.count)
+      while (5 > aidaG.count) {
+         totalM *= Double(Int(totalM))
+         break
+      }
+         totalM += Double(Int(totalM) << (NNECenterTool3.min(labs(2), 4)))
+      for _ in 0 ..< 3 {
+         indexH["\(aidaG)"] = 1 * aidaG.count
+      }
+      for _ in 0 ..< 2 {
+         indexH = ["\(totalM)": 2]
+      }
+         aidaG = "\(aidaG.count % (NNECenterTool3.max(4, Int(totalM))))"
+         totalM -= Double(Int(totalM) >> (NNECenterTool3.min(labs(1), 1)))
+         indexH["\(totalM)"] = 1 >> (NNECenterTool3.min(4, indexH.keys.count))
+      repeat {
+         indexH[aidaG] = aidaG.count
+         if indexH.count == 1882512 {
+            break
+         }
+      } while (indexH.count == 1882512) && (!aidaG.contains("\(indexH.count)"))
+      timelabeli.append("\(indexH.count * Int(totalM))")
+      break
+   }
+
+   if !voiceY.hasPrefix(timelabeli) {
+      timelabeli = "\((voiceY == (String(cString:[113,0], encoding: .utf8)!) ? voiceY.count : timelabeli.count))"
+   }
+        
+        detectNetworkStatus()
+   while (voiceY != timelabeli) {
+       var currentS: String! = String(cString: [100,101,99,111,114,97,116,105,111,110,115,0], encoding: .utf8)!
+       var contentZ: Int = 4
+       var apassQ: String! = String(cString: [109,97,99,114,111,115,0], encoding: .utf8)!
+       var liholderlabelB: String! = String(cString: [115,105,110,103,117,108,97,114,0], encoding: .utf8)!
+       var handingx: Float = 3.0
+      while (1.87 <= (Float(liholderlabelB.count) / (NNECenterTool3.max(7, handingx))) || 3 <= (Int(handingx) / (NNECenterTool3.max(liholderlabelB.count, 7)))) {
+          var objX: String! = String(cString: [117,99,111,110,115,116,0], encoding: .utf8)!
+          var ylabelZ: Int = 1
+          var uploadO: String! = String(cString: [103,101,110,101,114,97,116,101,0], encoding: .utf8)!
+          var aimagek: Double = 0.0
+         liholderlabelB.append("\(apassQ.count)")
+         objX.append("\(uploadO.count)")
+         ylabelZ /= NNECenterTool3.max(2, 2)
+         uploadO = "\(Int(aimagek) * 3)"
+         aimagek -= Double(ylabelZ)
+         break
+      }
+          var chooseR: Int = 3
+         handingx -= Float(1)
+         chooseR /= NNECenterTool3.max(3, 2)
+         liholderlabelB = "\(apassQ.count)"
+      repeat {
+          var execute6: Float = 1.0
+         liholderlabelB = "\(liholderlabelB.count ^ currentS.count)"
+         execute6 -= Float(1)
+         if (String(cString:[98,103,112,119,98,0], encoding: .utf8)!) == liholderlabelB {
+            break
+         }
+      } while ((contentZ | 1) == 2 && 1 == (contentZ | 1)) && ((String(cString:[98,103,112,119,98,0], encoding: .utf8)!) == liholderlabelB)
+         liholderlabelB = "\(1)"
+      repeat {
+         apassQ = "\(Int(handingx))"
+         if apassQ.count == 1480674 {
+            break
+         }
+      } while (apassQ.count == 1480674) && (apassQ.hasPrefix("\(handingx)"))
+         handingx += Float(contentZ + 3)
+         currentS.append("\(((String(cString:[68,0], encoding: .utf8)!) == apassQ ? currentS.count : apassQ.count))")
+          var userx: [Any]! = [704, 555, 740]
+          var handlerH: Bool = true
+         liholderlabelB.append("\(apassQ.count - 1)")
+         userx = [((handlerH ? 2 : 1) << (NNECenterTool3.min(userx.count, 3)))]
+         handlerH = userx.count <= 86 || handlerH
+      if liholderlabelB.count < currentS.count {
+          var main_kF: String! = String(cString: [97,99,113,117,105,114,101,0], encoding: .utf8)!
+         currentS.append("\(Int(handingx))")
+         main_kF.append("\(1 + main_kF.count)")
+      }
+          var ylabelA: Float = 4.0
+          var homeG: [String: Any]! = [String(cString: [99,111,115,116,105,0], encoding: .utf8)!:434, String(cString: [112,97,114,115,101,0], encoding: .utf8)!:871]
+          var servicea: String! = String(cString: [98,117,108,108,101,116,0], encoding: .utf8)!
+         handingx += Float(servicea.count)
+         ylabelA += Float(2 & Int(ylabelA))
+         homeG = ["\(homeG.keys.count)": homeG.keys.count & Int(ylabelA)]
+         servicea = "\(homeG.values.count)"
+       var linesV: Double = 5.0
+          var instanceM: Int = 3
+         handingx /= NNECenterTool3.max(Float(instanceM & 1), 5)
+      if 5 >= (contentZ >> (NNECenterTool3.min(labs(1), 4))) || (contentZ >> (NNECenterTool3.min(labs(1), 3))) >= 2 {
+         contentZ *= Int(handingx) | 3
+      }
+      repeat {
+         apassQ = "\(currentS.count >> (NNECenterTool3.min(5, labs(Int(handingx)))))"
+         if (String(cString:[50,55,107,50,95,51,121,0], encoding: .utf8)!) == apassQ {
+            break
+         }
+      } while (1 < (contentZ % (NNECenterTool3.max(7, apassQ.count))) || 1 < (contentZ % (NNECenterTool3.max(1, 2)))) && ((String(cString:[50,55,107,50,95,51,121,0], encoding: .utf8)!) == apassQ)
+      timelabeli.append("\(currentS.count % 3)")
+      break
+   }
+        
+        
+        IQKeyboardManager.shared().isEnabled = true
+        IQKeyboardManager.shared().shouldResignOnTouchOutside = true
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        CGKeTookingManager.shareTool(getAccountNumberIdentifier()!)
+        
+        if let first = UserDefaults.standard.object(forKey: "first") as? Int {
+            if first == 1 {
+
+                if UserDefaults.standard.object(forKey: "AccountToken") == nil {
+                    window?.rootViewController = UINavigationController(rootViewController: UNKRegisterController())
+                }
+                else {
+                    
+                    if let status = UserDefaults.standard.object(forKey: "loginStatus") as? Int {
+                        if status == 3 {
+                            window?.rootViewController = KVWAboutSpeedsController()
+                        }
+                        
+                        if status == 1 {
+                            window?.rootViewController = KVWAboutSpeedsController()
+                        }
+                        
+                        if status == 2 {
+                            if let goods = UserDefaults.standard.object(forKey: "goods") as? Int {
+                                
+                                if goods == 1 {
+                                    window?.rootViewController = KVWAboutSpeedsController()
+                                }
+                                else {
+                                    window?.rootViewController = UINavigationController(rootViewController: KMCardController())
+                                }
+                                
+                            }
+                            else {
+                                window?.rootViewController = KVWAboutSpeedsController()
+                            }
+                        }
+                    }
+                    else {
+                        
+                        window?.rootViewController = KVWAboutSpeedsController()
+                
+                    }
+                    
+                }
+
+            }
+            else {
+                window?.rootViewController = UINavigationController(rootViewController: DModityController())
+            }
+        }
+        else {
+            window?.rootViewController = UINavigationController(rootViewController: DModityController())
+        }
+        
+        
+        window?.makeKeyAndVisible()
+        
+        TXCommonHandler.sharedInstance().setAuthSDKInfo("yQm3sfmpFA6soPw50F0HA+9hAisxKFOgp3BEmYdWC56UzEHkK3EoNkllFF5snWODFNy2l+nsK4u6iI/q835rUGBPaNAvdQNlt48DIcQ36fqQ6yVLNA91LLnsxqGHWAWLVBtbFYsVscRipdbCgcDPd5oOSadxIfT8+o892cVzQJAe8+zNlxIre0Cq3Nys4w6y3NUsT0kLCy2bBp8WG384d/kmpGJDoRMGE5U45SytiTNbGFo6lqjb44tazQo4WyS1") { resultDic in
+
+            print("设置秘钥结果：\(resultDic)")
+        }
+        
+        return true
+    }
+
+}
+
+
+ struct NNECenterTool3 {
+    static func min<G: Comparable>(_ b1: G, _ b2: G) -> G {
+        return b1 > b2 ? b2 : b1
+    }
+    
+    static func max<G: Comparable>(_ b1: G, _ b2: G) -> G {
+        return b1 > b2 ? b1 : b2
+    }
+}
